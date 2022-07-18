296f186973ef20eeaca98a327e63e71814f543f4
Denys Vladymyrov <link_amd@yahoo.com>
2022-07-18 21:19:26 +0300

    added Firebase

---
+++ src/js/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAMnD_acU2a70lR62x4M3NbTUJRoifGH08",
+  authDomain: "noteballs-231cc.firebaseapp.com",
+  projectId: "noteballs-231cc",
+  storageBucket: "noteballs-231cc.appspot.com",
+  messagingSenderId: "178498724950",
+  appId: "1:178498724950:web:1f7191a56d070e3a8807f7"
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
+
+export {
+  db
+}
