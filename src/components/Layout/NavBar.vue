165c6c5d57d16e527067dbaebfa8e59c333b70a0
Denys Vladymyrov <link_amd@yahoo.com>
2022-07-20 11:47:06 +0300

    implemented basic auth

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -32,6 +32,15 @@
         :class="{ 'is-active' : showMobileNav }"
         ref="navbarMenuRef"
       >
+        <div class="navbar-start">
+          <button
+            v-if="storeAuth.user.id"
+            @click="logout"
+            class="button is-small is-info mt-3 ml-3"
+          >
+            Log out {{ storeAuth.user.email }}
+          </button>
+        </div>
         <div class="navbar-end">
           <RouterLink
             @click="showMobileNav = false"
@@ -62,6 +71,13 @@
 
   import { ref } from 'vue'
   import { onClickOutside } from '@vueuse/core'
+  import { useStoreAuth } from '@/stores/storeAuth'
+
+/*
+  store
+*/
+
+  const storeAuth = useStoreAuth()
 
 /*
   mobile nav
@@ -81,6 +97,15 @@
   }, {
     ignore: [navbarBurgerRef]
   })
+
+/*
+  logout
+*/
+
+  const logout = () => {
+    showMobileNav.value = false
+    storeAuth.logoutUser()
+  }
 
 </script>
 
--- src/js/firebase.js
+++ src/js/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: "AIzaSyAMnD_acU2a70lR62x4M3NbTUJRoifGH08",
@@ -12,7 +13,9 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app)
+const auth = getAuth(app)
 
 export {
-  db
+  db,
+  auth
 }
